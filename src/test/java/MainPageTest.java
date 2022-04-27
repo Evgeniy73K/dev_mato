5a28702a796a96f2a7a3a1973236906721384620
Evgeniy73K <evg73ul@gmail.com>
2022-04-27 17:04:56 +0400

    add autoscreenshot if a test failed

--- src/test/java/MainPageTest.java
+++ src/test/java/MainPageTest.java
@@ -1,18 +1,31 @@
+import junit.framework.TestResult;
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.runners.model.TestClass;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 
 public class MainPageTest {
 
     private WebDriver driver;
     private MainPage mainPage;
 
-    @Before
+    @BeforeMethod
     public void setUp() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -27,11 +40,24 @@ public class MainPageTest {
         RegisterPage registerPage = mainPage.registerPage();
         String heading = registerPage.geth1();
         Assert.assertEquals("Register for a new account", heading);
-
     }
+    
+    @AfterMethod
+    public void takeScreenshot(ITestResult result) {
+        if (! result.isSuccess()) {
+            File screen = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            Date date = new Date();
+            SimpleDateFormat format = new SimpleDateFormat("hh_mm_ss");
+            String name = format.format(date)+".png";
 
-    @After
-    public void tearDown(){
+            try {
+                FileUtils.copyFile(screen,new File("/home/evgeniy/Pictures/dev_mato"+name));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
         driver.quit();
     }
 
